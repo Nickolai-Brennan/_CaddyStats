64957f7fb01b17a3798ea23344eedbf2b63bb6ca
Nick Brennan <nsbrennan@outlook.com>
2026-02-21 06:39:53 -0500

    Add mutation to GraphQL schema

--- Backend/app/graphql/schema.py
+++ Backend/app/graphql/schema.py
@@ -1,17 +1,10 @@
+# Backend/app/graphql/schema.py
 import strawberry
-
 from app.graphql.queries import Query
 from app.graphql.mutations import Mutation
+from app.graphql.context import get_context  # you will create this
 
-@strawberry.type
-class Query:
-    @strawberry.field
-    def ping(self) -> str:
-        return "pong"
-
-    @strawberry.field
-    def server_time(self) -> str:
-        return datetime.utcnow().isoformat() + "Z"
-
-
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+)
